8efea05a742a083a44b8f544c4a80f8c10d88122
p238049y <p238049y@gmail.com>
2024-06-29 19:15:24 +0900

    [add] パスのエイリアスを追加

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
@@ -11,4 +12,10 @@ export default defineConfig({
     globals: true,
     environment: 'happy-dom',
   },
+  resolve: {
+    // @をエイリアスとして設定している
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
