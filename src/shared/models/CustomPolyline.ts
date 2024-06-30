be0db8f3425fce49b31afb2464043fd59ef5c5f1
Shimpei Iwamaru <gangan.education@gmail.com>
2024-06-30 22:18:16 +0900

    exp1

--- src/shared/models/CustomPolyline.ts
+++ src/shared/models/CustomPolyline.ts
@@ -27,3 +27,20 @@ export class CustomPolyline {
         }
     }
 }
+
+export function add(...args: number[]) {
+  return args.reduce((a, b) => a + b, 0);
+}
+
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest;
+  it('add: case1', () => {
+    expect(add()).toBe(0);
+  });
+  it('add: case2', () => {
+    expect(add(1)).toBe(1);
+  });
+  it('add: case3', () => {
+    expect(add(1, 2, 3)).toBe(6);
+  });
+}
