0a192152cc39ee570ae0c99ae6c1e7f4d476b8ab
p238049y <p238049y@gmail.com>
2024-06-30 21:52:27 +0900

    [add] #2 In-Source Testingをするための設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,16 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath } from 'url'
 
 export default defineConfig({
+  define : {
+    'import.meta.vitest': false,
+  },
   plugins: [
     vue()
   ],
   test: {
     globals: true,
     environment: 'happy-dom',
+    includeSource: ['src/**/*.{js,ts}'], 
   },
   resolve: {
     // @をエイリアスとして設定している
